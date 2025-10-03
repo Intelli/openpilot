77e012203d4fe3d642b90e6de0ef7d0d2c08057e
Intelli <contact@intelli.software>
2025-10-02 21:57:14 -0600

    don't store .pkl files in LFS

--- release/ci/publish.sh
+++ release/ci/publish.sh
@@ -41,6 +41,11 @@ cd $OUTPUT_DIR
 git init
 git lfs install --local
 git lfs track "*.pkl"
+git config lfs.skipSmudge 1
+git config lfs.fetchrecentrefsdays 3650
+git config lfs.fetchrecentcommitsdays 3650
+git config lfs.url https://gitlab.com/sunnypilot/public/sunnypilot-new-lfs.git/info/lfs
+git config lfs.pushurl ssh://git@gitlab.com/sunnypilot/public/sunnypilot-new-lfs.git
 
 # set git username/password
 #source /data/identity.sh
