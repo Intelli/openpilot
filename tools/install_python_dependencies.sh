e755133a21a6711d38f78586a8a00e4450e33f86
Intelli <contact@intelli.software>
2025-10-04 15:43:52 -0600

    Test

--- tools/install_python_dependencies.sh
+++ tools/install_python_dependencies.sh
@@ -19,8 +19,9 @@ echo "updating uv..."
 # ok to fail, can also fail due to installing with brew
 uv self update || true
 
-echo "installing python packages..."
-uv sync --frozen --all-extras
+UV_SYNC_FLAGS=${UV_SYNC_FLAGS:-"--frozen --all-extras"}
+echo "installing python packages with uv sync $UV_SYNC_FLAGS"
+uv sync $UV_SYNC_FLAGS
 source .venv/bin/activate
 
 if [[ "$(uname)" == 'Darwin' ]]; then
