836a2bb9add56efdd248103602d527b749ffce6c
Intelli <contact@intelli.software>
2025-10-08 16:09:51 -0600

    Drop UploadsActive from params_keys.h

--- common/params_keys.h
+++ common/params_keys.h
@@ -247,5 +247,4 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"HkgTuningAngleCustomLimitMaxSpeedKph", {PERSISTENT | BACKUP, INT, "40"}},
     {"HkgTuningEv9AlertsSpeedKph", {PERSISTENT | BACKUP, INT, "40"}},
     {"HkgTuningOverridingCycles", {PERSISTENT | BACKUP, INT, "17"}},
-    {"UploadsActive", {CLEAR_ON_MANAGER_START, BOOL}},
 };
