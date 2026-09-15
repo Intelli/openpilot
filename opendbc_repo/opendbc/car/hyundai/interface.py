f0f358fa95999b621cd7bddbac24ca94c14c7755
Intelli <contact@intelli.software>
2026-09-14 22:23:07 -0600

    opendbc: migrate steering saturation warning timing

--- opendbc_repo/opendbc/car/hyundai/interface.py
+++ opendbc_repo/opendbc/car/hyundai/interface.py
@@ -262,7 +262,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.centerToFront = ret.wheelbase * 0.4
     ret.steerActuatorDelay = 0.1
-    ret.steerLimitTimer = 0.4
+    ret.steerLimitTimer = 0.3  # Detect sustained steering saturation sooner.
     if not (ret.flags & HyundaiFlags.CANFD_ANGLE_STEERING):
       CarInterfaceBase.configure_torque_tune(candidate, ret.lateralTuning)
 
